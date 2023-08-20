aca62b96a057ff8f74e047a7d5e4c727bb4c83eb
Oleg Mihailik <mihailik@gmail.com>
2023-08-20 20:57:15 +0100

    THREE.js cube

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // @ts-check
+
+/** @type {import('three')} */
+var THREE;
+
 function bskye() {
 
   function loading() {
@@ -10,5 +14,48 @@ function bskye() {
     middle.appendChild(central);
   }
 
-  loading();
+  function createScene() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshLambertMaterial({ color: 0x4080FF });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    const remote = new THREE.BoxGeometry(40, 40, 1);
+    const remotePaint = new THREE.MeshLambertMaterial({ color: 0xFFFFFF });
+    const remoteCube = new THREE.Mesh(remote, remotePaint);
+    remoteCube.position.z = -5;
+    scene.add(remoteCube);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    scene.add(directionalLight);
+
+    const light = new THREE.HemisphereLight(0xDDDDFF, 0x09090A, 1);
+    scene.add(light);
+
+    camera.position.z = 5;
+
+    animate();
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.015;
+
+      renderer.render(scene, camera);
+    }
+  }
+
+  if (typeof THREE !== 'undefined') {
+    createScene();
+  } else {
+    loading();
+  }
 }
