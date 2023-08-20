fc7af377ea0a09b999fcb990b6c3fc8c347fe992
mihailik <mihailik@gmail.com>
2023-08-20 17:56:17 +0100

    Dummy loading page.

---
+++ index.js
@@ -0,0 +1,14 @@
+// @ts-check
+function bskye() {
+
+  function loading() {
+    var middle = document.createElement('div');
+    middle.style.cssText = 'position: absolute; top:0;left:0;width:100%;top:100%; display: grid; grid-template-row: 1fr; grid-template-column: 1fr; align-items: center; justify-items: center;';
+    document.body.appendChild(middle);
+    var central = document.createElement('div');
+    central.textContent = 'Loading...';
+    middle.appendChild(central);
+  }
+
+  loading();
+}
